2e180ae6792cddaef1da449ef95b61607b75c087
Ваше Имя <.email@example.com>
2024-04-13 18:54:17 +0300

    chnage path url

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: ['encrypted-tbn2.gstatic.com', 'encrypted-tbn0.gstatic.com'],
-	},
 	async rewrites() {
 		return [
 			{
-				source: '/api/:path*',
+				source: '/:path*',
 				destination: 'http://o-complex.com:1337/:path*',
 			},
 		]
